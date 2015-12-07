43a7c2f3fe1480a041299f4b2c9c6ea43ba6c8b6
Julien Sarazin <julien.sarazin@me.com>
2015-12-07 13:46:53 +0100

    updated uneven.js

--- sources/uneven.js
+++ sources/uneven.js
@@ -1,7 +1,7 @@
 module.exports = function(array){
 	var filteredItems = [];
 	for(var i=0; i<array.length; i++)
-		if (array[i] % 2 === 0)
+		if (array[i] % 2 !== 0)
 			filteredItems.push(array[i]);
 
 	return filteredItems;
