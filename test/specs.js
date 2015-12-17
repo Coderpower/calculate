be8d9a6de684e4955c35739d5f7c515de732fbff
Julien Sarazin <julien.sarazin@me.com>
2015-12-17 11:41:03 +0100

    updated sources

--- test/specs.js
+++ test/specs.js
@@ -5,21 +5,21 @@ var starts = require('../sources/starts');
 
 describe('contains', function(){
 	it('should validate text that contains word or not', function(){
-		expect(contains('Hello Coderpower', 'Coderpower')).to.be.true;
-		expect(contains('Hello Coderpower', 'Foo Bar')).to.be.false;
+		expect(contains('Hello Coderpower', 'Coderpower')).to.be.ok();
+		expect(contains('Hello Coderpower', 'Foo Bar')).to.not.be.ok();
 	});
 });
 
 describe('starts', function(){
 	it('should validate text that starts with word or not', function(){
-		expect(starts('Hello Coderpower', 'Hello')).to.be.true;
-		expect(starts('Hello Coderpower', 'Foo Bar')).to.be.false;
+		expect(starts('Hello Coderpower', 'Hello')).to.be.ok();
+		expect(starts('Hello Coderpower', 'Foo Bar')).to.not.be.ok();
 	});
 });
 
 describe('ends', function(){
 	it('should validate text that ends with word or not', function(){
-		expect(ends('Hello Coderpower', 'Coderpower')).to.be.true;
-		expect(ends('Hello Coderpower', 'Foo Bar')).to.be.false;
+		expect(ends('Hello Coderpower', 'Coderpower')).to.be.ok();
+		expect(ends('Hello Coderpower', 'Foo Bar')).to.not.be.ok();
 	});
 });
